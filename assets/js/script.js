9161f6fe21f989cb62f76646300e89350b42c9c4
Muhammad Qamar Abbas <136121713+muhammadqamar22092@users.noreply.github.com>
2024-07-03 02:34:48 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,7 +98,7 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
     for (let j = 0; j < pages.length; j++) {
-      if (this.innerHTMAL.toLowerCase() === pages[j].dataset.page) {
+      if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
         pages[j].classList.add("active");
         navigationLinks[j].classList.add("active");
         window.scrollTo(0, 0);
